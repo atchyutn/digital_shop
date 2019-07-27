6082559828876263c5b172b48609fcc8c9710428
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2019-07-27 20:50:05 +0530

    Added Product validations, associations and related specs

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+  # model associations
+  has_many :cart_items
+  belongs_to :user, class_name: "User", foreign_key: "created_by"
+  belongs_to :category
+
+  # model validations
+  validates :name, presence: true
+  validates :category_id, presence: true
+  validates :price, presence: true
+  validates :status, presence: true
+  validates :created_by, presence: true
+
 end

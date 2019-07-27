4da8ba8c6e6ac32f1612b46177f282aa9135d1eb
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2019-07-28 00:39:08 +0530

    Added CartItem associations, validation, related specs

--- spec/models/cart_item_spec.rb
+++ spec/models/cart_item_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe CartItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # association specs
+  it { should belong_to(:user) }
+  it { should belong_to(:purchase) }
+  it { should belong_to(:product) }
+
+  # validation specs
+  [:product_id, :order_id, :user_id, :price].each do |field|
+    it { should validate_presence_of(field) }
+  end
 end

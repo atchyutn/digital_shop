820783b0a048970e2423afecbef5a814b974e221
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2019-07-28 00:24:39 +0530

    Added tax model associations, validations and related specs

--- app/models/tax.rb
+++ app/models/tax.rb
@@ -1,2 +1,7 @@
 class Tax < ApplicationRecord
+  # model associations
+  belongs_to :purchase, class_name: "Purchase", foreign_key: "order_id"
+
+  # model validations
+  validates_presence_of :name, :percent, :order_id
 end
--- spec/models/tax_spec.rb
+++ spec/models/tax_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Tax, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # association specs
+  it { should belong_to(:purchase) }
+
+  # validation specs
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:percent) }
+  it { should validate_presence_of(:order_id) }
 end

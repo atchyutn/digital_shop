5ab8e3a9ad38b9fa7e02f59a919cbd663c913c7f
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2019-07-27 19:22:36 +0530

    title: Added categories model
    desc:
    1. Added categories model with three columns
    2. Generated a related spec file

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_27_081757) do
+ActiveRecord::Schema.define(version: 2019_07_27_135133) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.integer "created_by"
+    t.integer "updated_by"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

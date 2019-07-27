4d48ca83be3602bb010bdf2334ec4dcd0ebc6ec4
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2019-07-27 20:46:39 +0530

    Added Category associations and validations

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # association specs
+  it { should belong_to(:user) }
+  it { should have_many(:products).dependent(:destroy) }
+
+  # validation specs
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:created_by) }
 end

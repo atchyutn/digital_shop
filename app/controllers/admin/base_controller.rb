1619f557ca36aa7f5ce951af530dd10fd44c8601
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2019-08-04 11:18:22 +0530

    title: Added authorization
    desc:
    1. Added admin authorization method in the base control

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,14 @@
 class Admin::BaseController < ApplicationController
   protect_from_forgery with: :exception
 
-  before_action :authenticate_user!
+  before_action :authenticate_user! && :authorize_admin!
+
+  private
+
+  def authorize_admin!
+    return if current_user.role == '0' 
+    flash[:alert] = 'Login as Admin to access this page!'
+    redirect_to root_path
+  end
 
 end

9c8ac951fc761d7c667e1edcc6e896cf4c95b495
Atchyut Nagabhairava <atchyutnagabhairava@gmail.com>
2019-08-04 08:22:11 +0530

    title: Added User functionalities
    desc:
    1. User crud operations for admin
    2. Generated User devise pages
    3. Bootstraped User devise pages
    4. Added flash messages

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,42 @@
+class Admin::UsersController < Admin::BaseController
+  # before actions for user controller
+  before_action :find_user, only: [:edit, :update, :destroy]
+
+  # display all users
+  def index
+    @users = User.all.order(:created_at)
+  end
+
+  # edit specific user
+  def edit
+  end
+
+  # update specific user
+  def update
+    if @user.update(user_params)
+      flash[:success] = "Woohoo! User updated successfully."
+      redirect_to admin_users_path
+    else
+      render 'edit'
+    end
+  end
+
+  # delete specific user from database
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user}deleted successfully!"
+    redirect_to admin_users_path
+  end
+
+  private
+
+  # find user from id
+  def find_user
+    @user = User.find_by(id: params[:id])
+  end
+
+  # user params for user updation
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :role)
+  end
+end
